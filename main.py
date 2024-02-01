f7ca294c2e23aa779f3c74ccab10d54a8e18a7ac
Logan Boyd <loganboyd@gmail.com>
2024-02-01 11:04:15 -0600

    updating main

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
 import boto3
 import json
 
-# Use Amazon Rekognition for label detection in photos
-def detect_labels(photo):
-    client = boto3.client('rekognition')
 
-    with open(photo, 'rb') as image:
-        response = client.detection(Image={'Bytes': image.read()})
+# Use Amazon DynamoDB to retrieve a user's preferences
+def get_user_preferences(user_id) -> dict:
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('GenAI_AB')
 
-    return response['Labels']
+    response = table.get_item(Key={'username': user_id})
+
+    return response['Item'] if 'Item' in response else {}
 
 
-# create a function to save the labels to a json file
-def save_labels(labels):
-    with open('labels.json', 'w') as f:
-        json.dump(labels, f)
+# User Amazon Bedrock and the Anthropic Claude V2 model to generate the text for the user's profile bio
+def generate_bio_text(prefs) -> str:
+    bedrock = boto3.client('bedrock-runtime')   
 
+    body = {'prompt': f'Human: Create a bio for a dating profile for the Tinder app using first person tone of voice. The bio is for a man named Logan who is {prefs["age"]} years old who is seeking a woman. The man has the following interests: {prefs["interests"]}. He has the following hobbies: {prefs["hobbies"]}. He is looking for the following in a potential match: {prefs["others"]}. Assistant:',
+        'maxTokens': 300,
+        'temperature': 0.3  # Scale between 0 and 1 where smaller value means more deterministic and large value means more creative
+    }
+    print(body['prompt'], '\n\n')
+
+    response = bedrock.invoke_model(
+        modelId='ai21.j2-mid-v1',
+        body = json.dumps(body)        
+    )
+    response_body = json.loads(response.get("body").read())
+    response_text = response_body["completions"][0]["data"]["text"]
+    return response_text
+
+
+# Use Amazon Comprehend to do toxicity detection
+def detect_toxicity(text) -> str:
+    client = boto3.client('comprehend')
+
+    response = client.detect_toxic_content(TextSegments=text, LanguageCode='en')
+
+    return response
+
+
+# Use Amazon DynamoDB to update a user's profile bio
+def update_user_profile(user_id, bio) -> None:
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('GenAI_AB')
+
+    response = table.update_item(
+        Key={'username': user_id},
+        UpdateExpression='SET bio = :bio',
+        ExpressionAttributeValues={':bio': bio}
+    )
+
+    return response
 
 if __name__ == "__main__":
-    photo = 'IMG_0543.jpg'
-    labels = detect_labels(photo)
-    # print(labels)
-    print(len(labels))
-    print(labels[0]['Name'])
-    print(labels[0]['Confidence'])
-    print(labels[0]['Instances'])
-    print(labels[0]['Parents'])
-    print(labels[0]['Categories'])
 
-    save_labels(labels)
+    # Get User Profile Preferences
+    username = 'logan'
+    prefs = get_user_preferences(username)
+    print(prefs, '\n\n')
+
+    # Use Bedrock to Generate Bio
+    bio = generate_bio_text(prefs).strip()
+    print(bio, '\n\n')
+
+    # Check for Toxicity in Bio
+    bio_text = [{'Text': bio}]
+    toxicity = detect_toxicity(bio_text)
+    print(toxicity, '\n\n')
+
+    for label in toxicity['ResultList'][0]['Labels']:
+        if label['Score'] > 0.2:
+            print(f'The bio is toxic based on label: {label["Name"]}')
+            break
+    
+    # Update User Profile with New Bio
+    response = update_user_profile(username, bio)
+    print(response)
